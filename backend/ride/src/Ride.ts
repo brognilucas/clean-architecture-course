d666048248693c2c249bc02bebc739c3d10d8a45
Lucas Brogni <lucas.brogni@taxdoo.com>
2023-07-10 20:09:44 -0300

    calculate waiting time when creating Ride passing both accepted and requestedAt

--- backend/ride/src/Ride.ts
+++ backend/ride/src/Ride.ts
@@ -2,6 +2,8 @@ import { Location } from './Location';
 import { RideStatus } from './RideStatus';
 
 export default class Ride {
+  waitingTime: number = 0;
+
   constructor(
     readonly from: Location,
     readonly to: Location,
@@ -9,6 +11,13 @@ export default class Ride {
     readonly driver_id: string | null = null,
     readonly status: RideStatus = RideStatus.WAITING_DRIVER,
     readonly requestedAt: Date = new Date(),
+    readonly acceptedAt: Date | null = null,
     readonly id: string = ''
-  ){}
+  ){
+    if (this.acceptedAt && this.requestedAt){ 
+      this.waitingTime = this.acceptedAt.getTime() - this.requestedAt.getTime()
+    }
+  }
+
+
 }
--- backend/ride/test/Ride.test.ts
+++ backend/ride/test/Ride.test.ts
@@ -1,6 +1,7 @@
 import Ride from "../src/Ride";
+import { RideStatus } from "../src/RideStatus";
 
-it("should return an instance of Ride", () => { 
+it("should return an instance of Ride", () => {
 
   const ride = new Ride({
     lat: 10,
@@ -24,4 +25,19 @@ it("should ride have a default status of waiting_driver", () => {
   }, "passengerId");
 
   expect(ride.status).toBe("waiting_driver");
+})
+
+test("should calculate the waiting time, if passing acceptedAt and requestedAt", () => {
+  const acceptedAt = new Date();
+  const requestedAt = new Date(acceptedAt.getTime() - 1000 * 60 * 2);
+
+  const ride = new Ride({
+    lat: 10,
+    long: 20
+  }, {
+    lat: 30,
+    long: 40
+  }, "passengerId", "driverId", RideStatus.ACCEPTED, requestedAt, acceptedAt);
+
+  expect(ride.waitingTime).toBe(1000 * 60 * 2);
 })
